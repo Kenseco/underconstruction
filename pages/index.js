20dcd0a20ee3236091d08513d50e44f0b779f37f
Kenseco <kenseco@kooco.com.tw>
2021-11-17 18:49:36 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="糟糕,看起來不妙..." />
-        <div style="position: absolute;top: 100px;max-width: 100%;"> <img src="https://kooco.oss-cn-hongkong.aliyuncs.com/under_construct.png" with="600" heigh="400" alt="圖片"></div>
+        <div style="position: absolute;top: 100px;max-width: 100%;"> <img src="https://kooco.oss-cn-hongkong.aliyuncs.com/under_construct.png" with="600" heigh="400" alt="圖片"/></div>
        
       </main>
 
